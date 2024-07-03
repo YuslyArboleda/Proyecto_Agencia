13d7603a35e34d5a92db5c3fe75dfbb9899888df
Luis Palacios <luisedinpalacios16@gmail.com>
2024-07-03 09:03:24 -0500

    Cambios generales

---
+++ README.md
@@ -0,0 +1,9 @@
+# Proyecto_Agencia
+Instalar paquete mysql=>
+ - python --version
+ - pip install --upgrade pip
+ - pip install mysql-connector-python==3.12.3
+ - python -m venv myenv
+ - myenv\Scripts\activate
+ - pip install mysql-connector-python
+ - pip list
---
+++ luis_juan/template/conexion.py
@@ -0,0 +1,23 @@
+# Importar la biblioteca de conexión
+import mysql.connector
+
+# Establecer la conexión
+try:
+    connection = mysql.connector.connect(
+        host="localhost", user="root", password="1234", database="agencia_de_viaje_luis_edin_juan_romana"
+    )
+
+    if connection.is_connected():
+        print("Conexión exitosa a la base de datos")
+        cursor = connection.cursor()
+        cursor.execute("SELECT DATABASE();")
+        record = cursor.fetchone()
+        print("Conectado a la base de datos:", record)
+
+except mysql.connector.Error as err:
+    print(f"Error: {err}")
+finally:
+    if connection.is_connected():
+        cursor.close()
+        connection.close()
+        print("La conexión a la base de datos se ha cerrado.")
