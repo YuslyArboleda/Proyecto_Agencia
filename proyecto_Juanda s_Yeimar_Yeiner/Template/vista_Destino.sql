e1dbc3a3e4a4c2052af83415003169e4648633d2
Yeiner2003 <heinercusta@gmail.com>
2024-07-10 08:59:44 -0500

    Tabla destino, permiso usuario, vistas y procedimientos

--- proyecto_Juanda s_Yeimar_Yeiner/Template/vista_Destino.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-        `destino`.`id_Destino` AS `id_Destino`,
-        `destino`.`Departamento` AS `Departamento`,
-        `destino`.`Ciudad` AS `Ciudad`
-    FROM
-        `destino`
-    WHERE
-        (`destino`.`estado` = '1')
