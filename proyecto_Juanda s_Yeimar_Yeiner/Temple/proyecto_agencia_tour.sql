f78bea0e671b794c055776674ad8ab2f71aa69a6
juanda136 <juandasepur@gmail.com>
2024-07-10 10:27:38 -0500

    Base de datos Juan Sepulveda

--- proyecto_Juanda s_Yeimar_Yeiner/Temple/proyecto_agencia_tour.sql
+++ proyecto_Juanda s_Yeimar_Yeiner/Temple/proyecto_agencia_tour.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `proyecto_agencia` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `proyecto_agencia`;
 -- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
 -- Host: localhost    Database: proyecto_agencia
@@ -26,28 +24,17 @@ DROP TABLE IF EXISTS `tour`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tour` (
   `id_tour` int NOT NULL AUTO_INCREMENT,
+  `nombre_tour` varchar(50) NOT NULL,
+  `descripcion` varchar(200) NOT NULL,
+  `valor` float NOT NULL,
+  `duracion` int NOT NULL,
   `id_destino` int NOT NULL,
-  `nombre_tour` varchar(45) NOT NULL,
-  `valor` decimal(10,2) NOT NULL,
-  `fecha_de_inicio` date NOT NULL,
-  `fecha_final` date NOT NULL,
-  `descripcion` text NOT NULL,
-  `disponibilidad` int NOT NULL,
+  `estado` tinyint NOT NULL,
   PRIMARY KEY (`id_tour`),
   KEY `destino_idx` (`id_destino`),
   CONSTRAINT `destino` FOREIGN KEY (`id_destino`) REFERENCES `destino` (`id_Destino`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tour`
---
-
-LOCK TABLES `tour` WRITE;
-/*!40000 ALTER TABLE `tour` DISABLE KEYS */;
-INSERT INTO `tour` VALUES (10,123,'C',14500.00,'2024-10-10','2024-10-12','dadjfadskj',10);
-/*!40000 ALTER TABLE `tour` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -58,4 +45,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-07-10  9:09:06
+-- Dump completed on 2024-07-10 10:27:00
