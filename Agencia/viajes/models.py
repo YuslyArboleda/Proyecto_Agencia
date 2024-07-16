3acd8044142c97d8c36f22ffb19318bec8e6e328
Jean <oofawe@gmail.com>
2024-07-16 10:16:30 -0500

    subiendo jean_jhoan

--- Agencia/viajes/models.py
+++ Agencia/viajes/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-<<<<<<< HEAD
 
-class Costos (models.Model):
+
+class Paquete (models.Model):
     nombre = models.CharField (max_length = 200)
     descripcion = models.CharField (max_length = 500)
     fecha_inicio = models.DateField()
@@ -12,17 +12,17 @@ class Costos (models.Model):
     costo = models.FloatField()
     estado = models.BooleanField()
     disponibilidad = models.CharField (max_length = 50)
-=======
+
 class Cliente(models.Model):
-    nombre =models.CharField(max_length=45),
-    telefono =models.CharField(max_length=45),
-    correo =models.EmailField(max_length=45),
-    direccion =models.CharField(max_length=45),
-    cantidad =models.CharField(max_length=45),
+    nombre =models.CharField(max_length=45)
+    telefono =models.CharField(max_length=45)
+    correo =models.EmailField(max_length=45)
+    direccion =models.CharField(max_length=45)
+    cantidad =models.CharField(max_length=45)
     estado =models.BooleanField(),
 
 
 
->>>>>>> a55a0caaf641ad9d310c36b1ff461911b1a4271c
+
 
 
