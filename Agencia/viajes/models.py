bbeec9b7e9b3b0b240477a5380b0cafe7ef349ca
Jean <oofawe@gmail.com>
2024-07-16 10:34:01 -0500

    subiendo archivos jhoan

--- Agencia/viajes/models.py
+++ Agencia/viajes/models.py
@@ -13,13 +13,6 @@ class Paquete (models.Model):
     estado = models.BooleanField()
     disponibilidad = models.CharField (max_length = 50)
 
-class Cliente(models.Model):
-    nombre =models.CharField(max_length=45)
-    telefono =models.CharField(max_length=45)
-    correo =models.EmailField(max_length=45)
-    direccion =models.CharField(max_length=45)
-    cantidad =models.CharField(max_length=45)
-    estado =models.BooleanField(),
 
 
 
