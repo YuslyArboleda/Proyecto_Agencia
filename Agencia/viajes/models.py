9a1940c9ebf07083ce04142b238ba205283a881d
pedro luis cordoba <Pedro luis Cordoba>
2024-07-16 09:24:55 -0500

    se agrego el campo tipo documento

--- Agencia/viajes/models.py
+++ Agencia/viajes/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 class Cliente(models.Model):
-    nombre =models.CharField(max_length=45),
-    telefono =models.CharField(max_length=45),
-    correo =models.EmailField(max_length=45),
-    direccion =models.CharField(max_length=45),
-    cantidad =models.CharField(max_length=45),
-    estado =models.BooleanField(),
+    nombre =models.CharField(max_length=200)
+    Tipo_doc =models.CharField(max_length=20)
+    telefono =models.CharField(max_length=20)
+    correo =models.EmailField(max_length=200)
+    direccion =models.CharField(max_length=50)
+    cantidad =models.CharField(max_length=45)
+    estado =models.BooleanField()
 
 
 
