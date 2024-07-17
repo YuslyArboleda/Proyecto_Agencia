d422b64a6a8c07d72853865672d98d68e1fff2e2
Jean <oofawe@gmail.com>
2024-07-17 10:14:56 -0500

    haciendo pull jhoan

--- Agencia/viajes/models.py
+++ Agencia/viajes/models.py
@@ -1,26 +1,4 @@
-from django.db import models
 
-class Paquete (models.Model):
-    nombre = models.CharField (max_length = 200)
-    descripcion = models.CharField (max_length = 500)
-    fecha_inicio = models.DateField()
-    fecha_fin = models.DateField()
-    noche = models.IntegerField()
-    incluye = models.CharField (max_length = 200)
-    no_incluye = models.CharField (max_length = 200)
-    costo = models.FloatField()
-    estado = models.BooleanField()
-    disponibilidad = models.CharField (max_length = 50)
-
-
-class Cliente(models.Model):
-    nombre =models.CharField(max_length=200)
-    Tipo_doc =models.CharField(max_length=20)
-    telefono =models.CharField(max_length=20)
-    correo =models.EmailField(max_length=200)
-    direccion =models.CharField(max_length=50)
-    cantidad =models.CharField(max_length=45)
-    estado =models.BooleanField()
 
 
 
