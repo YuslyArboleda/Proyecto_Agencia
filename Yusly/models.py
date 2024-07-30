3767135bcaa0e71b6e0c9354ccbb4f83bc2207d1
Yusly Arboleda <yuslyarboleda1@gmail.com>
2024-07-30 09:41:55 -0500

    Add files via upload

---
+++ Yusly/models.py
@@ -0,0 +1,318 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+from django.db import models
+
+
+class Acomodacion(models.Model):
+    id_acomodacion = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=300)
+    descripcion = models.CharField(max_length=300)
+    estado = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'acomodacion'
+
+
+class Adicion(models.Model):
+    id_adicion = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=20)
+    descripcion = models.CharField(max_length=200)
+    costo = models.FloatField()
+    estado = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'adicion'
+
+
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.IntegerField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.IntegerField()
+    is_active = models.IntegerField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
+
+class Cliente(models.Model):
+    id_cliente = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=200)
+    tipo_doc = models.CharField(max_length=20)
+    documento = models.CharField(unique=True, max_length=20)
+    telefono = models.CharField(max_length=20)
+    correo = models.CharField(max_length=200)
+    direccion = models.CharField(max_length=50)
+    id_destino = models.ForeignKey('Destino', models.DO_NOTHING, db_column='id_destino')
+    estado = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'cliente'
+
+
+class Destino(models.Model):
+    id_destino = models.AutoField(primary_key=True)
+    destino = models.CharField(max_length=200)
+    estado = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'destino'
+
+
+class DetalleReserva(models.Model):
+    id_detalle_reserva = models.AutoField(primary_key=True)
+    id_reserva = models.ForeignKey('Reserva', models.DO_NOTHING, db_column='id_reserva')
+    id_paquete_tour = models.ForeignKey('PaqueteTour', models.DO_NOTHING, db_column='id_paquete_tour', blank=True, null=True)
+    id_acomodacion = models.ForeignKey(Acomodacion, models.DO_NOTHING, db_column='id_acomodacion', blank=True, null=True)
+    id_adicion = models.ForeignKey(Adicion, models.DO_NOTHING, db_column='id_adicion', blank=True, null=True)
+    habitaciones = models.IntegerField()
+    adulto = models.IntegerField()
+    infante = models.IntegerField()
+    comentarios = models.CharField(max_length=500, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'detalle_reserva'
+
+
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.PositiveSmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
+
+
+class Hospedaje(models.Model):
+    id_hospedaje = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=50)
+    correo = models.CharField(max_length=30)
+    tipo_hospedaje = models.CharField(max_length=30)
+    descripcion = models.CharField(max_length=100)
+    tarifa_base = models.FloatField()
+    id_destino = models.ForeignKey(Destino, models.DO_NOTHING, db_column='id_destino')
+    estado = models.IntegerField()
+    imagen = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'hospedaje'
+
+
+class HospedajeAcomodacion(models.Model):
+    id_hospedaje_acomodacion = models.AutoField(primary_key=True)
+    id_acomodacion = models.ForeignKey(Acomodacion, models.DO_NOTHING, db_column='id_acomodacion')
+    id_hospedaje = models.ForeignKey(Hospedaje, models.DO_NOTHING, db_column='id_hospedaje')
+    temporada = models.CharField(max_length=20)
+    tarifa_agencia = models.FloatField()
+    tarifa = models.FloatField()
+    imagen = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'hospedaje_acomodacion'
+
+
+class Paquete(models.Model):
+    id_paquete = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=200)
+    descripcion = models.CharField(max_length=500)
+    vigencia_inicio = models.DateField()
+    vigencia_fin = models.DateField()
+    noche = models.IntegerField()
+    incluye = models.CharField(max_length=200)
+    no_incluye = models.CharField(max_length=200)
+    costo = models.FloatField()
+    disponibilidad = models.CharField(max_length=50)
+    estado = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'paquete'
+
+
+class PaqueteTour(models.Model):
+    id_paquete_tour = models.AutoField(primary_key=True)
+    id_paquete = models.ForeignKey(Paquete, models.DO_NOTHING, db_column='id_paquete')
+    id_tour = models.ForeignKey('Tour', models.DO_NOTHING, db_column='id_tour')
+
+    class Meta:
+        managed = False
+        db_table = 'paquete_tour'
+
+
+class Permiso(models.Model):
+    id_permiso = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    estado = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'permiso'
+
+
+class PermisoUsuario(models.Model):
+    id_permiso_usuario = models.AutoField(primary_key=True)
+    id_permiso = models.ForeignKey(Permiso, models.DO_NOTHING, db_column='id_permiso')
+    id_usuario = models.ForeignKey('Usuario', models.DO_NOTHING, db_column='id_usuario')
+
+    class Meta:
+        managed = False
+        db_table = 'permiso_usuario'
+
+
+class Reserva(models.Model):
+    id_reserva = models.AutoField(primary_key=True)
+    fecha = models.DateField()
+    fecha_inicio = models.DateField()
+    fecha_fin = models.DateField()
+    cantidad = models.IntegerField()
+    total = models.FloatField()
+    estado_reserva = models.CharField(max_length=20)
+    metodo_pago = models.CharField(max_length=20)
+    id_usuario = models.ForeignKey('Usuario', models.DO_NOTHING, db_column='id_usuario')
+    estado = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'reserva'
+
+
+class Tour(models.Model):
+    id_tour = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=200, blank=True, null=True)
+    valor = models.FloatField()
+    duracion = models.IntegerField()
+    estado = models.IntegerField()
+    id_destino = models.ForeignKey(Destino, models.DO_NOTHING, db_column='id_destino')
+    imagen = models.TextField()
+
+    class Meta:
+        managed = False
+        db_table = 'tour'
+
+
+class Usuario(models.Model):
+    id_usuario = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=200)
+    tipo_doc = models.CharField(max_length=20)
+    documento = models.CharField(max_length=20)
+    telefono = models.CharField(max_length=20)
+    correo = models.CharField(max_length=200)
+    sexo = models.IntegerField()
+    direccion = models.CharField(max_length=50)
+    rol = models.IntegerField()
+    login = models.CharField(max_length=30)
+    clave = models.CharField(max_length=40)
+    fecha_creacion = models.DateField()
+    estado = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'usuario'
